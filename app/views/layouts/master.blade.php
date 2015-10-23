1eb8a4a060881eaa8acb4f06e1f65029dc6a71b2
appsmambo <quintanilla.peru@gmail.com>
2015-10-23 10:35:42 -0500

    analytics y palazzos

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -51,6 +51,15 @@
 		<script src="{{url('js/jquery.bxslider.min.js')}}"></script>
 		<script src="{{url('js/jquery.fancybox.pack.js')}}"></script>
 		<script src="{{url('js/main.js?v=10')}}"></script>
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+			ga('create', 'UA-50197344-4', 'auto');
+			ga('require', 'displayfeatures');
+			ga('send', 'pageview');
+		</script>
 	</head>
 	<body class="{{$clase}}">
 		<h1 class="hidden">
