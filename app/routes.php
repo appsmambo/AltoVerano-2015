836804f2b437cc40b5316279733807573626c9f5
appsmambo <quintanilla.peru@gmail.com>
2015-10-28 17:04:02 -0500

    quitar sandalias y manejador de error

--- app/routes.php
+++ app/routes.php
@@ -4,9 +4,10 @@ Route::get('/', array('as' => 'index', 'uses' => 'HomeController@getIndex'));
 Route::get('/top-5-del-verano', array('as' => 'top5', 'uses' => 'HomeController@getTop5'));
 Route::get('/tu-look-por-menos', array('as' => 'tuLook', 'uses' => 'HomeController@getTuLook'));
 
-
+/*
 Route::get('/sandalias', array('as' => 'Sandalias', 'uses' => 'HomeController@getSandalias'));
 Route::get('/sandalias/nueva-temporada', array('as' => 'nuevaTemporada', 'uses' => 'HomeController@getSandaliasNuevaTemporada'));
 Route::get('/sandalias/ninos', array('as' => 'ninos', 'uses' => 'HomeController@getSandaliasNinos'));
 Route::get('/sandalias/accesorios', array('as' => 'accesorios', 'uses' => 'HomeController@getSandaliasAccesorios'));
 Route::get('/sandalias/sandalias-por-menos', array('as' => 'sandaliasPorMenos', 'uses' => 'HomeController@getSandaliasPorMenos'));
+*/
